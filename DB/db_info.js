f13adc7603af6febd51150d838a01dcd1456ed59
남아현 <43693054+Ahyun1023@users.noreply.github.com>
2020-03-19 16:54:25 +0900

    Update db_info.js

--- DB/db_info.js
+++ DB/db_info.js
@@ -4,8 +4,8 @@ module.exports = (()=> {
             host: 'localhost',
             port: 3306,
             user: 'root',
-            password: 'fkrtmgkswks',
-            database: 'blog'
+            password: '*************',
+            database: 'board'
         },
 
         real: { //실 서버용
